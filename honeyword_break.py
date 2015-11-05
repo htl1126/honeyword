a6b2154baa835ba77b27694d71fe15df1b48dcf7
Shreyas <ssk272@cornell.edu>
2015-11-04 22:31:36 -0500

    WIP

--- honeyword_break.py
+++ honeyword_break.py
@@ -2,7 +2,6 @@ import sys
 import csv
 from difflib import SequenceMatcher
 import re
-import string
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
@@ -64,59 +63,6 @@ def findNumberOfDigitsAddedAtTheBeginning(wordList):
             numDigitDict[key] = 1
     return numDigitDict
 
-def hasSpecialChars(str):
-    return any(char in set(string.punctuation) for char in str)
-
-#take in input text file, ouput a dictionary of special characters {1, 20; 2, 30...etc}
-#NOTE that for key = 4, that accounts for 4 or more special characters
-def freqSpecial(input):
-    freqDict = dict([(0,0),(1, 0),(2,0),(3,0),(4,0)])
-    for i, wordList in enumerate(input):
-        numSpecChar = 0
-        for word in wordList:
-            for letter in word:
-                if hasSpecialChars(letter):
-                    numSpecChar += 1
-            if (numSpecChar > 4):
-                freqDict[4] += 1
-            else:      
-                freqDict[numSpecChar]+= 1
-    return freqDict
-
-def readRockYouData(size=None, filename='rockyou-withcount.txt'):
-    word_lst = []
-    count = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            line = line.strip()
-            word = line.split(' ')[1]
-            word_lst.append(word)
-            count += 1
-            if size and count == size:
-                break
-    return word_lst
-
-
-def findMostSimilarStringFromPool(inputFile):
-    cut_off_val = 0.9
-    string_pool = readRockYouData(100)
-    output_string_pool =[]
-    result=dict()
-    for line in inputFile :
-        for word in line:
-            output_string_pool.append( difflib.get_close_matches(word, string_pool,
-                                                   cutoff=cut_off_val))
-    if len(output_string_pool) and inputFile != output_string_pool[0]:
-        for x in output_string_pool:
-            for y in x:
-                if y in result:
-                    result[y]+=1
-                else:
-                    result[y]=1
-        return result
-    else:
-        return None
-
 def read_data(filename, num_set, num_sweetword):
     data = []
     with open(filename, 'r') as f:
@@ -157,13 +103,15 @@ def findPatternForNoise(input_data):
 def guess_honeyword(input_data):
     pass
  
+
 def main(num_sweetword, num_set, filename):
     #input_data = read_data(filename, num_set, num_sweetword)
     input_data = read_txt(filename)
     inputArray = parseDataArray(input_data)
-    print findNumberOfFirstCharCapitalizedWords(inputArray)
-    print findNumberOfDigitsAddedAtTheBeginning(inputArray)
-    print findNumberOfDigitsAddedAtTheEnd(inputArray)
+    capDict = findNumberOfFirstCharCapitalizedWords(inputArray)
+    begDigitDic = findNumberOfDigitsAddedAtTheBeginning(inputArray)
+    endDigitDic = findNumberOfDigitsAddedAtTheEnd(inputArray)
+    
     # guesses = guess_honeyword(input_data)
     # for guess in guesses:
     #     print guess
